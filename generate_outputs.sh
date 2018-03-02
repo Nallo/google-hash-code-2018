bee64b925f5f4eb339a38c29b7a2825c3465d98d
Your Name <you@example.com>
2018-03-02 20:17:32 +0100

    generate outputs

---
+++ generate_outputs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python main.py < a_example.in > a_example.out
+python main.py < b_should_be_easy.in > b_should_be_easy.out
+python main.py < c_no_hurry.in > c_no_hurry.out
+python main.py < d_metropolis.in > d_metropolis.out
+python main.py < e_high_bonus.in > e_high_bonus.out
